82aa960dad626c31751120543a57e040aa34ee83
Samat <Asylbekuulusamat555@gmail.com>
2021-03-09 16:02:04 +0600

    Component and first layout Second day

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = ()=>{
+  return(
+<header className="header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUYhTowF5WCHLLtUAN_CoWdSRo1m4pq37tNw&usqp=CAU" />
+    </header>
+    )
+}
+      
+export default Header;
